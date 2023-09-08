c3a7cc296389f8061fca4b3d840b4fc625ac6932
Jenrola Odunolaoluwa <jenrola2292@gmail.com>
2023-09-08 03:36:57 +0100

    more corrctions

--- apis/main.py
+++ apis/main.py
@@ -5,10 +5,23 @@ from transformers import (
     BitsAndBytesConfig)
 
 
-model_path ='./llamamodel'
-tokenizer_path = './llamatokenizer'
+model_path ='meta-llama/Llama-2-7b-chat-hf'
+tokenizer_path = 'meta-llama/Llama-2-7b-chat-hf'
 
-model = AutoModelForCausalLM.from_pretrained(model_path)
+bnb_config = BitsAndBytesConfig(
+    load_in_8bit=True,
+    bnb_4bit_quant_type= 'nf4',
+    bnb_4bit_compute_dtype = torch.float16,
+    bnb_4bit_use_double_quant=False
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    quantization_config = bnb_config,
+    device_map = 'auto',
+    
+)
+
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
 app = FastAPI()
@@ -28,4 +41,7 @@ def check_load():
 @app.post('/predict')
 def predict(text):
     prompt = template.format(question = text)
+    tokens = tokenizer(prompt, return_tensors = 'pt')
+    output = model.generate(**tokens, max_length = 500).split('[/INST]')[-1]
+    return output
 
