c4fd4520640d710905f2a2fe58fd17847153dcf5
Jenrola Odunolaoluwa <jenrola2292@gmail.com>
2023-09-09 07:07:37 +0100

    added support for sentence similarity

--- apis/main.py
+++ apis/main.py
@@ -4,28 +4,39 @@ from transformers import (
     AutoTokenizer,
     BitsAndBytesConfig)
 import torch
+from sentence_transformers import SentenceTransformer
+import weaviate
+
+client = weaviate.Client(
+      url="https://testingserver-8otaf3tj.weaviate.network",
+      additional_headers={
+          "X-HuggingFace-Api-Key": "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"
+      }
+  )
 
 model_path ='meta-llama/Llama-2-7b-chat-hf'
 tokenizer_path = 'meta-llama/Llama-2-7b-chat-hf'
 
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
-    bnb_4bit_quant_type= 'nf4',
+    bnb_4bit_quant_dtype= 'nf4',
     bnb_4bit_compute_dtype = torch.float16,
     bnb_4bit_use_double_quant=False
 )
 
-model = AutoModelForCausalLM.from_pretrained(
+llm_model = AutoModelForCausalLM.from_pretrained(
     model_path,
     quantization_config = bnb_config,
     device_map = 'auto',
     
 )
-
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
+
+retriever_model = SentenceTransformer('BAAI/bge-large-en')
+
 app = FastAPI()
-template = """
+llm_template = """
 [INST] <<SYS>>
 You are an helpful and smart assistant who knows his way around the kitchen and answers User's question as best as you can
 <</SYS>>
@@ -40,9 +51,25 @@ def check_load():
 
 @app.post('/predict')
 def predict(text):
-    prompt = template.format(question = text)
+    prompt = llm_template.format(question = text)
     tokens = tokenizer(prompt, return_tensors = 'pt')
-    outputs = model.generate(**tokens, max_length = 500)[0]
+    outputs = llm_model.generate(**tokens, max_length = 500)[0]
     response = tokenizer.decode(outputs, skip_special_tokens=True).split('[/INST]')[-1]
     return response
+
+@app.post('/retrievedb')
+def retrieve_from_vector_db(request):
+    query = f"To generate a representation for this sentence for use in retrieving related articles: {request}"
+    query_vector = retriever_model.encode(query)
+    response = client.query.get(
+    "Recipes",
+    ["texts"]
+        ).with_limit(3).with_near_vector(
+            {'vector': query_vector}
+        ).do()
+    return response['data']['Get']['Recipes']
+
+
+
+
 
