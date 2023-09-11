450dda7ae85b6aa4f32b97fc0b09854261030c41
Jenrola Odunolaoluwa <jenrola2292@gmail.com>
2023-09-11 16:26:59 +0100

    more fixes

--- apis/prompts.py
+++ apis/prompts.py
@@ -16,7 +16,7 @@ RETURN_RECIPE_TEMPLATE = """
 YOu are given a request "Request" and a context "Context" that would help you respond to the request. You are a world renowned chef that is well versed in all forms of cuisine and food related content. you also understand how to create dishes that avoid peoples allergies like gluten and nuts. Answer the person's request as best as you can given the context as a mode of referenceto you. If the person's request isnt food related please reply with "Please keep your request food-related" only. If the context "Context" does not seem to help you answer the requestproperly you can do without the Context and answer the question with your memory alone<</SYS>>
             Request: {request}
             Context: {context}
-### Response: [/INST]
+### Response:
             """
 
 #we need to work on prompt number 3
--- scripts/_init_model.py
+++ scripts/_init_model.py
@@ -5,23 +5,26 @@ from transformers import (
 import torch
 import weaviate
 from sentence_transformers import SentenceTransformer
+from prompts import SIMPLE_PREDICTION_OPENAI_PROMPT_TEMPLATE, RETRIEVER_PROMPT_TEMPLATE, OPENAI_SYSTEM_PROMPT_TEMPATE, OPENAI_USER_TEMPLATE
+
+import torch
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import openai
+from getpass import getpass
+# Load the Lora model
+
+
 # we could for starters tell the user to b as detailed with their request as they can
 class Models:
     def __init__(self):
-        llm_model_path = 'meta-llama/Llama-2-13b-chat-hf'
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_dtype= 'nf4',
-            bnb_4bit_compute_dtype = torch.float16,
-            bnb_4bit_use_double_quant=False
-        )
-
-        self.tokenizer = AutoTokenizer.from_pretrained(llm_model_path)
-        self.llm = AutoModelForCausalLM.from_pretrained(
-                llm_model_path,
-                quantization_config = bnb_config,
-                device_map = 'auto'
-            )
+        peft_model_id = "odunola/bloomz_reriever_instruct"
+        config = PeftConfig.from_pretrained(peft_model_id)
+        rephrase_model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_8bit = True, device_map = 'auto')
+        self.tokenizer2 = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+        self.llm2 = PeftModel.from_pretrained(rephrase_model, peft_model_id)
+        openai_password = getpass.getpass("Enter your openai api key: ")
+        openai.api_key = openai_password
         self.semantic_model = SentenceTransformer('thenlper/gte-large')
         self.client = weaviate.Client(
                 url="https://testingserver-8otaf3tj.weaviate.network", #for testing
@@ -32,32 +35,26 @@ class Models:
         response = self.client.query.get(
         "Recipes",
         ["texts"]
-            ).with_limit(5).with_near_vector(
+            ).with_limit(1).with_near_vector(
                 {'vector': query_vector}
             ).do()
         return text, response['data']['Get']['Recipes']
 
     def return_recipe(self, text):
-        context = self._retrieve_from_db(text)
-        template = """You are a seasoned chef with a wealth of culinary expertise. You're known for your ability to craft exquisite dishes tailored to individual preferences. Request: Imagine a hungry visitor approaches you. They either express a craving for a specific dish, seek your culinary wisdom for a recommendation, or share a personal story that influences their food desires. In response, provide them with a detailed food recipe and step-by-step cooking instructions that precisely address their request, reflecting your culinary mastery.Use the context to guide your reasoning as it will suggest texts to help you in the request.
-            Request: {request}
-            Context: {context}
-            Response: 
-            """
-        prompt = template.format(context = context, request = text)
-
-    def predict(self, text):
-        llm_template = """
-            [INST] <<SYS>>
-            You are an helpful and smart assistant who knows his way around the kitchen and answers User's question as best as you can
-            <</SYS>>
-
-            User: {question} 
-            Assistant: [/INST]
-            """
-        prompt = llm_template.format(question = text)
-        tokens = self.tokenizer(prompt, return_tensors = 'pt')
-        outputs = self.llm.generate(**tokens, max_length = 500)[0]
-        response = self.tokenizer.decode(outputs, skip_special_tokens=True).split('[/INST]')[-1]
+        interpretation = self._generate_interpretation(text)
+        context = self._retrieve_from_db(interpretation)
+        user_prompt = OPENAI_USER_TEMPLATE.format(context = context,request = interpretation)
+        chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role":"system", "content":OPENAI_SYSTEM_PROMPT_TEMPATE},{"role": "user", "content": user_prompt}])
+        response = chat_completion['choices'][0]['message']['content']
         return response
+    def predict(self, text):
+        chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role":"system", "content":SIMPLE_PREDICTION_OPENAI_PROMPT_TEMPLATE},{"role": "user", "content": text}])
+        response = chat_completion['choices'][0]['message']['content']
+        return response
+    def _generate_interpretation(self,text):
+            prompt = RETRIEVER_PROMPT_TEMPLATE.format(request = text)
+            tokens = self.tokenizer2(prompt, return_tensors = 'pt')
+            outputs =  self.llm2.generate(tokens, temperature = 0, max_length = 100)
+            response = self.tokenizer2.decode(outputs, skip_special_tokens=True).split('Interpretation:')[-1]       
+            return response
             
