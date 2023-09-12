1263ddbff27991a7c665626e9aa302182690f3b2
Jenrola Odunolaoluwa <jenrola2292@gmail.com>
2023-09-12 14:29:40 +0100

    new notebook

---
+++ apis/interface.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+streamlit run --server.address 0.0.0.0 interface.py
--- apis/run.sh
+++ apis/run.sh
@@ -2,6 +2,6 @@
 
 pip install -r requirements.txt
 pip install -q git+https://github.com/huggingface/peft.git git+https://github.com/huggingface/transformers.git
-streamlit run --server.address 0.0.0.0 interface.py
+
 uvicorn main:app --reload --host 0.0.0.0 --port 4000
 
