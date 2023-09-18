5d59fcab35743d3fd42d1ec2841fb066454c88cb
odunola <jenrola2292@gmail.com>
2023-09-18 23:51:35 +0100

    Update run.sh

--- apis/run.sh
+++ apis/run.sh
@@ -2,5 +2,7 @@
 
 pip install -r requirements.txt
 pip install -q git+https://github.com/huggingface/peft.git git+https://github.com/huggingface/transformers.git
-uvicorn main:app --reload --host 0.0.0.0 --port 4000 --loop asyncio
+uvicorn main:app  --host 0.0.0.0 --port 4000 --loop asyncio &
+sleep 5
+streamlit run interface.py
 
